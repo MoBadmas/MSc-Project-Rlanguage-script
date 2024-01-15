ca6cc97d39ce9ae08152993b0e1629aed0c8dc95
Badamasi Mohammed <32996363+MoBadmas@users.noreply.github.com>
2024-01-15 12:57:54 +0100

    Update Analysis Code Script.R

--- Survey Analysis Code/Analysis Code Script.R
+++ Survey Analysis Code/Analysis Code Script.R
@@ -1,63 +1,60 @@
-# Before label mapping, remeber to calculate the  mean, median, mode, standard deviation, and variance. 
-
-
-
+# Before label mapping, remember to calculate the  mean, median, mode, standard deviation, and variance. 
 #Label Mapping for Survey Responses
-Test_Data$Q1...1 <- factor(Test_Data$Q1...1, levels = c(1, 2, 3, 4, 5, 6, 7),
+Survey_Data$Q1...1 <- factor(Survey_Data$Q1...1, levels = c(1, 2, 3, 4, 5, 6, 7),
                            labels = c("Under 18", "18-24", "25-34", "35-44", "45-54", "55-64", "65 and above"))
 
-Test_Data$Q2...2 <- factor(Test_Data$Q2...2, levels = c(1, 2, 3, 4),
+Survey_Data$Q2...2 <- factor(Survey_Data$Q2...2, levels = c(1, 2, 3, 4),
                            labels = c("Male", "Female", "Non-binary", "Prefer not to say"))
 
-Test_Data$Q3...3 <- factor(Test_Data$Q3...3, levels = c(1, 2, 3, 4, 5, 6, 7),
+Survey_Data$Q3...3 <- factor(Survey_Data$Q3...3, levels = c(1, 2, 3, 4, 5, 6, 7),
                            labels = c("No formal education", "Some High School", "High School Graduate", "Some College", "Bachelor's Degree", "Master's Degree", "Doctorate or higher" ))
 
-Test_Data$Q4...4 <- factor(Test_Data$Q4...4, levels = c(1, 2, 3, 4, 5, 6),
+Survey_Data$Q4...4 <- factor(Survey_Data$Q4...4, levels = c(1, 2, 3, 4, 5, 6),
                            labels = c("Employed Full-Time", "Employed Part-Time", "Unemployed", "Student", "Retired", "Homemaker" ))
 
-Test_Data$Q5...5 <- factor(Test_Data$Q5...5, levels = c(1, 2, 3, 4, 5),
+Survey_Data$Q5...5 <- factor(Survey_Data$Q5...5, levels = c(1, 2, 3, 4, 5),
                            labels = c("Daily", "Weekly", "Monthly", "Rarely", "Never" ))
 
-Test_Data$Q1...6 <- factor(Test_Data$Q1...6, levels = c(1, 2, 3, 4, 5),
+Survey_Data$Q1...6 <- factor(Survey_Data$Q1...6, levels = c(1, 2, 3, 4, 5),
                            labels = c("None at all", "A little", "A moderate amount", "A lot", "A great deal" ))
 
-Test_Data$Q2...7 <- factor(Test_Data$Q2...7, levels = c(1, 2, 3, 4, 5),
+Survey_Data$Q2...7 <- factor(Survey_Data$Q2...7, levels = c(1, 2, 3, 4, 5),
                            labels = c("Extremely inadequate", "Somewhat inadequate", "Neither adequate nor inadequate", "Somewhat adequate", "Extremely adequate" ))
 
-Test_Data$Q3...8 <- factor(Test_Data$Q3...8, levels = c(1, 2, 3, 4, 5),
+Survey_Data$Q3...8 <- factor(Survey_Data$Q3...8, levels = c(1, 2, 3, 4, 5),
                            labels = c("Strongly disagree", "Somewhat disagree", "Neither agree nor disagree", "Somewhat agree", "Strongly agree" ))
 
-Test_Data$Q4...9 <- factor(Test_Data$Q4...9, levels = c(1, 2, 3, 4, 5),
+Survey_Data$Q4...9 <- factor(Survey_Data$Q4...9, levels = c(1, 2, 3, 4, 5),
                            labels = c("Strongly disagree", "Somewhat disagree", "Neither agree nor disagree", "Somewhat agree", "Strongly agree" ))
 
-Test_Data$Q5...10 <- factor(Test_Data$Q5...10, levels = c(1, 2, 3, 4, 5),
+Survey_Data$Q5...10 <- factor(Survey_Data$Q5...10, levels = c(1, 2, 3, 4, 5),
                             labels = c("Strongly disagree", "Somewhat disagree", "Neither agree nor disagree", "Somewhat agree", "Strongly agree" ))
 
-Test_Data$Q6 <- factor(Test_Data$Q6, levels = c(1, 2, 3, 4, 5),
+Survey_Data$Q6 <- factor(Survey_Data$Q6, levels = c(1, 2, 3, 4, 5),
                        labels = c("Extremely uncomfortable", "Somewhat uncomfortable", "Neither comfortable nor uncomfortable", "Somewhat comfortable", "Extremely comfortable" ))
 
-Test_Data$Q7 <- factor(Test_Data$Q7, levels = c(1, 2, 3, 4, 5),
+Survey_Data$Q7 <- factor(Survey_Data$Q7, levels = c(1, 2, 3, 4, 5),
                        labels = c("Strongly disagree", "Somewhat disagree", "Neither agree nor disagree", "Somewhat agree", "Strongly agree" ))
 
-Test_Data$Q8 <- factor(Test_Data$Q8, levels = c(1, 2, 3, 4, 5),
+Survey_Data$Q8 <- factor(Survey_Data$Q8, levels = c(1, 2, 3, 4, 5),
                        labels = c("Strongly disagree", "Somewhat disagree", "Neither agree nor disagree", "Somewhat agree", "Strongly agree" ))
 
-Test_Data$Q9 <- factor(Test_Data$Q9, levels = c(1, 2, 3, 4, 5),
+Survey_Data$Q9 <- factor(Survey_Data$Q9, levels = c(1, 2, 3, 4, 5),
                        labels = c("Strongly disagree", "Somewhat disagree", "Neither agree nor disagree", "Somewhat agree", "Strongly agree" ))
 
-Test_Data$Q10 <- factor(Test_Data$Q10, levels = c(1, 2, 3, 4, 5),
+Survey_Data$Q10 <- factor(Survey_Data$Q10, levels = c(1, 2, 3, 4, 5),
                         labels = c("Strongly disagree", "Somewhat disagree", "Neither agree nor disagree", "Somewhat agree", "Strongly agree" ))
 
-Test_Data$Q11 <- factor(Test_Data$Q11, levels = c(1, 2, 3, 4, 5),
+Survey_Data$Q11 <- factor(Survey_Data$Q11, levels = c(1, 2, 3, 4, 5),
                         labels = c("Strongly disagree", "Somewhat disagree", "Neither agree nor disagree", "Somewhat agree", "Strongly agree" ))
 
-Test_Data$Q12 <- factor(Test_Data$Q12, levels = c(1, 2, 3, 4, 5),
+Survey_Data$Q12 <- factor(Survey_Data$Q12, levels = c(1, 2, 3, 4, 5),
                         labels = c("Strongly disagree", "Somewhat disagree", "Neither agree nor disagree", "Somewhat agree", "Strongly agree" ))
 
-Test_Data$Q13 <- factor(Test_Data$Q13, levels = c(1, 2, 3, 4, 5),
+Survey_Data$Q13 <- factor(Survey_Data$Q13, levels = c(1, 2, 3, 4, 5),
                         labels = c("Strongly disagree", "Somewhat disagree", "Neither agree nor disagree", "Somewhat agree", "Strongly agree" ))
 
-summary (Test_Data)
+summary (Survey_Data)
 
 
 
@@ -78,65 +75,45 @@ create_frequency_table <- function(data, variable_name) {
 }
 
 # usage for a variable 'Q1...1'
-frequency_table_Q1 <- create_frequency_table(Test_Data, "Q1...1")
+frequency_table_Q1 <- create_frequency_table(Survey_Data, "Q1...1")
 print(frequency_table_Q1)
 
 # Section 1: Demographic Question
-create_frequency_table(Test_Data, "Q1...1")
-
-create_frequency_table(Test_Data, "Q2...2")
-
-create_frequency_table(Test_Data, "Q3...3")
-
-create_frequency_table(Test_Data, "Q4...4")
-
-create_frequency_table(Test_Data, "Q5...5")
+create_frequency_table(Survey_Data, "Q1...1")
+create_frequency_table(Survey_Data, "Q2...2")
+create_frequency_table(Survey_Data, "Q3...3")
+create_frequency_table(Survey_Data, "Q4...4")
+create_frequency_table(Survey_Data, "Q5...5")
 
 # Section 2: Privacy Concerns
-create_frequency_table(Test_Data, "Q1...6")
-
-create_frequency_table(Test_Data, "Q2...7")
-
-create_frequency_table(Test_Data, "Q3...8")
-
-create_frequency_table(Test_Data, "Q4...9")
-
+create_frequency_table(Survey_Data, "Q1...6")
+create_frequency_table(Survey_Data, "Q2...7")
+create_frequency_table(Survey_Data, "Q3...8")
+create_frequency_table(Survey_Data, "Q4...9")
 
 # Section 3: Trust Towards Online Service Providers
-create_frequency_table(Test_Data, "Q5...10")
-
-create_frequency_table(Test_Data, "Q6")
-
-create_frequency_table(Test_Data, "Q7")
+create_frequency_table(Survey_Data, "Q5...10")
+create_frequency_table(Survey_Data, "Q6")
+create_frequency_table(Survey_Data, "Q7")
 
 # Section 4: Awareness of Ethical Considerations
-create_frequency_table(Test_Data, "Q8")
-
-create_frequency_table(Test_Data, "Q9")
-
-create_frequency_table(Test_Data, "Q10")
-
+create_frequency_table(Survey_Data, "Q8")
+create_frequency_table(Survey_Data, "Q9")
+create_frequency_table(Survey_Data, "Q10")
 
 # Section 5: General Concerns and Perceptions
-create_frequency_table(Test_Data, "Q11")
-
-create_frequency_table(Test_Data, "Q12")
-
-create_frequency_table(Test_Data, "Q13")
-
-colnames(Test_Data)
-
-
+create_frequency_table(Survey_Data, "Q11")
+create_frequency_table(Survey_Data, "Q12")
+create_frequency_table(Survey_Data, "Q13")
+colnames(Survey_Data)
 
 # To Visualize each question first install the ggplot2 package
 install.packages("ggplot2")
-
 # Repeat for each question
 library(forcats)
 library(tidyr) 
 library(dplyr)
 library(ggplot2)
-
 # A named vector to map column names to more descriptive titles
 titles <- c(Q1...1 = "Distribution of Age", 
             Q2...2 = "Distribution of Gender", 
@@ -184,7 +161,7 @@ create_plot <- function(data, column_name, title, factor_levels) {
 
 # Loop over each column and create a plot
 plots <- lapply(names(titles), function(col) {
-  create_plot(Test_Data, col, titles[col])
+  create_plot(Survey_Data, col, titles[col])
 })
 
 # Now you can view each plot individually
@@ -195,9 +172,23 @@ print(plots[[1]])
 ggsave("plot1.png", plots[[1]], width = 10, height = 8, dpi = 300)
 
 
+# Load necessary library
+library(dplyr)
+
+# Convert age and concerned columns to factor (if they are not already)
+Survey_Data$Q1 <- as.factor(Survey_Data$Q1)
+Survey_Data$Q6 <- as.factor(Survey_Data$Q6)
+
+# Performing ANOVA test
+anova_result <- aov(Q6 ~ Q1, data = Survey_Data)
+summary(anova_result)
 
 
+# Create a contingency table
+table <- table(Survey_Data$Q3, Survey_Data$Q8)
 
+# Perform Chi-Square Test of Independence
+chi_square_test <- chisq.test(table)
 
-
-
+# Output the results
+print(chi_square_test)
